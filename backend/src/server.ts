621d14a0a7202611dd76d04899a3ac2af6e48c3e
Sugandha <sugandhacj@gmail.com>
2025-10-30 14:29:20 +0100

    commit-two

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,8 +6,19 @@ import studentRoutes from "./routes/studentsRouter";
 const app: Express = express();
 const PORT: number = parseInt(process.env.PORT || "5000");
 // Middleware
-app.use(cors());
+//app.use(cors());
+
+// CORS Konfiguration
+const corsOptions = {
+ origin: process.env.FRONTEND_URL || "http://localhost:5173",
+ credentials: true
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+
+
 // Routen
 app.use("/api/students", studentRoutes);
 // ✅ Konzept: Funktions-Typisierung mit async
